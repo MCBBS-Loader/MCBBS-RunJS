0a9960d139c2eed789d422a2ea97b4e0b9238a7b
@s❤Rainbow <Twilight_Princess_Sparkle@outlook.com>
2020-11-13 23:06:56 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,31 @@
+// MCBBS-Module
+// @id io.github.mcbbs-loader.runjs
+// @name RunJS
+// @icon https://www.picbed.cn/images/2020/11/13/OIP.jpg
+// @author <a href='https://github.com/MCBBS-Loader' target='_blank'>MCBBS-Loader</a>
+// -MCBBS-Module
+$("div.blockcode").each((i, eld) => {
+  $(eld)
+    .children()
+    .each((i, el) => {
+      if ($(el).attr("id")?.startsWith("code")) {
+        $(eld).append("<em class='runcode'>运行代码</em>");
+        $(eld)
+          .children("em.runcode")
+          .attr("onclick", parseCode($(el)));
+      }
+    });
+});
+function parseCode(el) {
+  var strb = "";
+  var cs = el.children().children().get();
+  for (var l of cs) {
+    strb += $(l)
+      .html()
+      .replace(/<br>/g, "\n")
+      .replace(/&nbsp;/g, " ")
+      .replace(/&gt;/g, ">")
+      .replace(/&lt;/g, "<");
+  }
+  return strb;
+}
